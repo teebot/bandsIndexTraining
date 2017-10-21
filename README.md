b536d855a7f97fade91ff8ada7f0d7d908fc372b
Thibaut Nguyen <thibautnguyen@gmail.com>
2017-10-21 16:28:50 +0200

    Server in typescript, dev script with nodemon and concurrently

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bands Index
+
+A boilerplate for react and express in typescript
+
+## Install
+
+npm i
+
+## Run
+
+npm start
+
+## Develop
+
+npm run dev
--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 const app = express();
 
 app.set('view engine', 'pug');
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('static'));
 
-const data = require('./data/bandsIndex.json');
+const data = require('../data/bandsIndex.json');
 
 app.get('/api/bands/:bandId', (req, res) => {
     const band = data.bands.find(b => b.id === parseInt(req.params.bandId));
